10d6ec3729835b72855b255c34ad3aaef9704c17
IanKim <ihwan1236@gmail.com>
2025-01-29 19:52:27 -0800

    first commit with first project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+function App() {
+  return (
+    <div className="App">
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;

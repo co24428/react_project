d2bb0d76553bf1cace36718510bc49159607eb13
IanKim <ihwan1236@gmail.com>
2025-02-06 16:26:59 -0800

    Make Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,14 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router';
-import { Link } from "react-router-dom"
-
-// import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.tsx';
+import Profile from './components/Profile.tsx';
+import Home from './components/Home.tsx';
+import Login from './components/Login.tsx';
 import './App.css';
 
-// function App() {
 function App() {
-  // Navbar component
-  function Navbar() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <Link to = "/">Home</Link>
-          </li>
-          <li>
-            <Link to = "/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to = "/login">Login</Link>
-          </li>
-        </ul>
-        
-      </nav>
-    );
-  }
-
-  function Profile() {
-    return (
-      <div>
-        <h1>Profile</h1>
-        <p>Welcome to your profile page!</p>
-      </div>
-    );
-  }
-
-  const Home = () => {
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Welcome to the home page!</p>
-      </div>
-    );
-  }
-
-  function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    return (
-      <div>
-        <h1>Login</h1>
-        <form>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)} />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        <p>{username}</p>
-        <p>{password}</p>
-      </div>
-    );
-  }
-
-  // App's return
   return (
     <>
-    <Profile />
       <Navbar />
       <Routes>
         <Route path="/profile" element={<Profile />} />
